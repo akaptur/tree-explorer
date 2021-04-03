f961728b90f570042163c052c821937eb7758e14
Allison Kaptur <allison@pilot.com>
2021-04-03 12:25:00 -0700

    Is this how dependencies work?

--- experiment.js
+++ experiment.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+const { parseComponent } = require("vue-sfc-parser");
+
 const code = fs.readFileSync("/Users/akaptur/src/pilot/connections/client/views/admin/customer-chart-of-accounts-nav.vue", 'utf8');
 const parsed = parseComponent(code);
 console.log(parsed);
